e5aac20bf1e36ca0cf71f49b5d5c64815116704b
Elisee-des <esabidani@gmail.com>
2023-05-24 12:45:15 +0000

    GEstion des importation public

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from '@/pages/public/Home';
-import Service from '@/pages/public/Service';
-import Contact from '@/pages/public/Contact';
-import Error from '@/_utils/Error'
-import Layout from '@/pages/public/Layout';
+import PublicRouter from '@/pages/public/PublicRouter';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route element={<Layout/>} >
-            <Route index element={<Home/>} />
-            <Route path='/home' element={<Home/>} />
-            <Route path='/service' element={<Service/>} />
-            <Route path='/contact' element={<Contact/>} />
-            <Route path='*' element={ <Error/> } />
-          </Route>
+          <Route path='/*' element={<PublicRouter/>} />
         </Routes>
       </BrowserRouter>
     </div>
