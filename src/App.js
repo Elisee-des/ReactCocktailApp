0c6446185a18d57ddef90cb91a3a92b8a19aceaa
Elisee-des <esabidani@gmail.com>
2023-05-24 12:14:14 +0000

    Public

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from '@/pages/public/Home';
+import Service from '@/pages/public/Service';
+import Contact from '@/pages/public/Contact';
+import Error from '@/_utils/Error'
+import Layout from '@/pages/public/Layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout/>} >
+            <Route index element={<Home/>} />
+            <Route path='/home' element={<Home/>} />
+            <Route path='/service' element={<Service/>} />
+            <Route path='/contact' element={<Contact/>} />
+            <Route path='*' element={ <Error/> } />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
